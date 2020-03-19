40e6b49b1496e471304402e17eb64106b8fa9fb0
Batenov <shuric80@gmail.com>
2020-03-19 10:55:23 +0300

    second result

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -8,38 +8,32 @@ import styles from './Calculator.module.scss';
 function Calculator(props){
   const {errors, touched} = props;
   const [isShow, setShow] = useState(false);
-  const total  = [100, 300, 500, 1000, 5000, 10000, 50000, 100000];
-  const years = [{value:1,title:'1 Year'},
-                 {value:2, title:'2 Years'},
-                 {value: 3, title:'3 Years'},
-                 {value: 4, title:'4 Years'},
-                 {value: 5, title:'5 Years'},
-                 {value: 6, title:'6 Years'},
-                 {value: 7, title:'7 Years'},
-                 {value: 8, title:'8 Years'},
-                 {value: 9, title:'9 Years'},
-                 {value: 10, title:'10 Years'}
-                ];
-  const profits = [0, 10, 25, 50, 75, 100];
-  const risk = [
+  const arr_initialInvestman  = [100, 300, 500, 1000, 5000, 10000, 50000, 100000];
+
+  const [initialInvestmen, setInitialInvestman] = useState(arr_initialInvestman[0]);
+  const arr_years = ['1 Year', '2 Years', '3 Years', '4 Years', '5 Years', '6 Years', '7 Years', '8 Years', '9 Years', '10 Years'];
+  const [years, setYears] = useState(0);
+
+  const arr_Reinvest = [0, 10, 25, 50, 75, 100];
+  const [reinvest, setReinvest] = useState(0);
+
+  const arr_riskLevel = [
     { title: 'Low Risk (0.22% per day on avg.).', value:0.22},
     { title: 'Medium Risk (0.257% per day on avg.).', value: 0.257},
     { title:'High Risk (0.29% per day on avg.).', value:'0.29'}
   ];
-  const reinvest = [
-    { title: 'First day of each week', value: 0},
-    { title: 'First of each month', value: 1 },
-    { title: 'Each time the value of profits value a certain package(choose below)', value:2}
-  ];
-  const amount = [100, 300, 500, 1000, 5000, 10000, 50000, 100000];
-  const header_first_row = [
-    {title:'Initial Investment', value:'$1000'},
-    {title:'Risk Level', value:'High (0.29% per day on avg.)'},
-    {title:'Reinvestment %', value:'100%'},
-    {title:'Reinvestment Term', value:'When balance > $100'},
-    {title:'Projection Years', value:'more 5'}];
-  const header_second_row = [ 'Total Profits', 'Total Reinvestment','Net Profit','ROI', 'Starting Date'];
-  const [tm, setTm] = useState(new Date().toISOString().split('T')[0]);
+  const [riskLevel, setRiskLevel] = useState(0);
+
+  const sarr_Reinvest = ['First day of each week', 'First of each month', 'Each time the value of profits value a certain package(choose below)'];
+  const [sreinvest, ssetReinvest] = useState(0);
+
+  const arr_reinvestment_term = [100, 300, 500, 1000, 5000, 10000, 50000, 100000];
+  const [reinvestment_term, setReinvestmentTerm] = useState(arr_reinvestment_term[0]);
+
+  const [starting_day, setStartingDay] = useState(new Date().toISOString().split('T')[0]);
+  const [total_profit, setTotalProfit] = useState(0);
+
+
   const isMobile = window.innerWidth <= 800;
 
 
@@ -52,34 +46,34 @@ function Calculator(props){
             <div className={cn(isMobile ? styles.FlexColumn: styles.FlexRow)}>
               <div className={cn(styles.FlexCell)}>
 
-                <label for='totalPayID'>Initial Investment<span className={cn(styles.Required)}>*</span></label>
-                <select name="totalPay" id="totalPayID">
-                  { total.map((value) => (<option value={value}>$ {value}</option>))}
+                <label for='initialD'>Initial Investment<span className={cn(styles.Required)}>*</span></label>
+                <select name="initialID" id="initialPayID" onChange={(item)=>{setInitialInvestman(item.target.value)}}>
+                  { arr_initialInvestman.map((value) => (<option value={value}>$ {value}</option>))}
                 </select>
               </div>
               <div className={cn(styles.FlexCell)}>
                 <label for="riskFieldID">Risk Level<span className={cn(styles.Required)}>*</span>
                 </label>
-                <select name='riskField'id="riskFieldID">
-                  {risk.map((item) => (<option value={item.value}>{item.title}</option>)) }
+                <select name='riskField'id="riskFieldID" onChange={(event)=>{setRiskLevel(event.target.value)}}>
+                  {arr_riskLevel.map((item, index) => (<option value={index}>{item.title}</option>)) }
                 </select>
               </div>
             </div>
             <div className={cn(isMobile ? styles.FlexColumn: styles.FlexRow)}>
               <div className={cn(styles.FlexCell)}>
 
-                <label for="profitIdPay">What % of your profits would you like to reinvest?<span className={cn(styles.Required)}>*</span>
+                <label for="profitId">What % of your profits would you like to reinvest?<span className={cn(styles.Required)}>*</span>
                 </label>
 
-                <select id='profitIdPay'>
-                  {profits.map((value) => (<option value={value}>{value}%</option>)) }
+                <select id='reinvestId' onChange={(event)=>{setReinvest(event.target.value)}}>
+                  {arr_Reinvest.map((item, index) => (<option value={index}>{item}%</option>)) }
                 </select>
               </div>
               <div className={cn(isMobile ? styles.FlexColumn: styles.FlexRow)}>
                 <div className={cn(styles.FlexCell)}>
                   <label for="reinvestFieldID">How often would you like to reinvest?<span className={cn(styles.Required)}>*</span></label>
-                  <select id="reinvestFieldID" onChange={(event)=>{setShow(event.target.value==2)}}>
-                    {reinvest.map((item)=>(<option value={item.value}>{item.title}</option>))}
+                  <select id="reinvestFieldID" onChange={(event)=>{setShow(event.target.value==2); setReinvest(event.target.value)}}>
+                    {arr_Reinvest.map((item, index)=>(<option value={index}>{item}</option>))}
                   </select>
                 </div>
               </div>
@@ -91,17 +85,17 @@ function Calculator(props){
 
                  <label for="amounFieldID">If my balance reaches this amount, reinvest with this package<span className={cn(styles.Required)}>*</span>
                  </label>
-                 <select id="amounFieldID">
-                   {amount.map((item)=>(<option value="{item}">${item}</option>))}
+                 <select id="amounFieldID" onChange={(event)=>{setReinvestmentTerm(event.target.value)}}>
+                   {arr_reinvestment_term.map((item)=>(<option value="{item}">${item}</option>))}
                  </select>
                </div>
               }
 
               <div className={cn(isMobile ? styles.FlexRow: styles.Column)}>
                 <div className={cn(styles.FlexCell)}>
-                  <label for="protectionFieldID">Projection years<span className={cn(styles.Required)}>*</span></label>
-                  <select id="protectionFieldID">
-                    {years.map((item)=>(<option value={item.value}>{item.title}</option>))}
+                  <label for="yearsID">Projection years<span className={cn(styles.Required)}>*</span></label>
+                  <select id="yearsID" onChange={(item)=>{setYears(item.target.value)}}>
+                    {arr_years.map((item, index)=>(<option value={index}>{item}</option>))}
                   </select>
                 </div>
               </div>
@@ -109,10 +103,10 @@ function Calculator(props){
             <div className={cn(styles.FlexRow)}>
 
               <div className={cn(styles.FlexCell)}>
-                <label for="periodIdPay">Starting Date<span className={cn(styles.Required)}>*</span>
+                <label for="beginDateId">Starting Date<span className={cn(styles.Required)}>*</span>
                 </label>
                 <div className={cn(styles.FlexRow)}>
-                  <input value={tm} name='beginDate' type='date' onChange={(e)=>{setTm(e.target.value)}} id='beginDateId' />
+                  <input value={starting_day} name='beginDate' type='date' onChange={(e)=>{setStartingDay(e.target.value)}} id='beginDateId' />
                   <div className={cn(styles.CalendarIcon)}></div>
                 </div>
               </div>
@@ -138,10 +132,18 @@ function Calculator(props){
         <div className={cn(styles.HeaderResult)}><h2>Result</h2>
           <p>Note: Avg. daily profits are calculated based on 0.29%/Day, based on current investors data</p>
           <div className={cn(styles.HeaderResultFirstRow, styles.HeaderResultMixin)}>
-            {header_first_row.map((item)=>(<div className={cn(styles.ResultCell)}><h3>{item.title}</h3><p>{item.value}</p></div>))}
+            <div className={cn(styles.ResultCell)}><h3>Initial Investment</h3><p>{initialInvestmen}</p></div>
+            <div className={cn(styles.ResultCell)}><h3>Risk Level</h3><p>{arr_riskLevel[riskLevel].title}</p></div>
+            <div className={cn(styles.ResultCell)}><h3>Reinvestment %</h3><p>{arr_Reinvest[reinvest]}%</p></div>
+            <div className={cn(styles.ResultCell)}><h3>Reinvestment Term</h3><p>When balance > {reinvestment_term}</p></div>
+            <div className={cn(styles.ResultCell)}><h3>Projection Years</h3><p>{arr_years[years]}</p></div>
           </div>
           <div className={cn(styles.HeaderResultSecondRow, styles.HeaderResultMixin)}>
-            {header_second_row.map((item)=>(<div className={cn(styles.ResultCell)}><h3>{item}</h3><p></p></div>))}
+            <div className={cn(styles.ResultCell)}><h3>Total Profits</h3><p>{total_profit}</p></div>
+            <div className={cn(styles.ResultCell)}><h3>Total Reinvestment</h3><p>{total_profit}</p></div>
+            <div className={cn(styles.ResultCell)}><h3>Net Profit</h3><p>{total_profit}</p></div>
+            <div className={cn(styles.ResultCell)}><h3>ROI</h3><p>{total_profit}</p></div>
+            <div className={cn(styles.ResultCell)}><h3>Starting Date</h3><p>{starting_day}</p></div>
           </div>
         </div>
       </div>
