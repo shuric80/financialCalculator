98af511b254edbc8650a6323b37d60fe4d20cf2b
Batenov <shuric80@gmail.com>
2020-03-22 19:44:25 +0300

    Input component

---
+++ src/components/calculator/input.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import cn from 'classnames/bind';
+import PropType from 'prop-types';
+
+import styles from './Input.module.scss';
+
+function Input(props) {
+  const {
+    title, isRequired, handler, values,
+  } = props;
+  return (
+    <div className={cn(styles.Input)}>
+      <span className={cn(styles.Title)}>
+        {title}
+        {isRequired && <span className={cn(styles.RequiredElement)}>*</span> }
+      </span>
+      <select onChange={handler}>
+        { values.map((item, index) => (<option value={index}>{item}</option>))}
+      </select>
+    </div>
+  );
+}
+
+Input.propTypes = {
+  title: PropType.string.isRequired,
+  values: PropType.arrayOf(PropType.string).isRequired,
+  handler: PropType.func.isRequired,
+  isRequired: PropType.bool,
+};
+
+Input.defaultProps = {
+  isRequired: false,
+};
+
+export default Input;
